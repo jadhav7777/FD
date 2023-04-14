2740fe8e2d10328cefc4db9998b7af482e41727a
Abhishek_Jadhav_ <abhishek.jadhav@flexagon.com>
2023-04-14 16:36:24 +0530

    Updating process.sh file

--- simple-bash-scripts/process.sh
+++ simple-bash-scripts/process.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+program=Process-Detail
+version=1.0
+year=2023
+developer=FD
+
+echo "$program $version"
+echo "Copyright $year $developer. All rights reserved."
 echo "Hello $USER"
 echo "Hey i am $USER and will be telling you about the current processes"
 echo "Running processes List"
