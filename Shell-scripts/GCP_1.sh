a23bf2cb057ac7ec229873542ae594dca3194626
Abhishek_Jadhav_ <abhishek.jadhav@flexagon.com>
2023-05-23 16:15:11 +0530

    Update script

--- Shell-scripts/GCP_1.sh
+++
@@ -1,5 +0,0 @@
-gcloud compute instances describe instance-1 --zone us-central1-a
-echo "show gcp version"
-gcloud -v
-echo "Show @ again instances"
-gcloud compute instances describe instance-1 --zone us-central1-a
