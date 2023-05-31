6c64e87e75d8512507fe449cec3bd29e43b0d803
Abhishek_Jadhav_ <abhishek.jadhav@flexagon.com>
2023-05-31 12:27:56 +0530

    update.

--- PowerShell-Azure-Scripts/Create-Azure-VM.ps1
+++ PowerShell-Azure-Scripts/Create-Azure-VM.ps1
@@ -1,14 +1,5 @@
-$creds = [System.Management.Automation.PSCredential]::new($ClientId, (ConvertTo-SecureString $ClientSecret -AsPlainText -Force))
-$ResourceGroupName = "Abhishek_REsource_Group"  
-$location = "EastUS"  
-$VMName = "Nginx-VM"
-$ImageName = "acr0510.azurecr.io/abhishekjadhav/test_acr:latest"
-
-Connect-AzAccount -Tenant $TenantId -Subscription $SubscriptionId -Credential $creds -ServicePrincipal | Out-Null
-Write-Information "Connected to Azure..."
-
-$password = ConvertTo-SecureString "FD@123azure" -AsPlainText -Force
-$CredVM = New-Object System.Management.Automation.PSCredential ("abhishek", $password)
+$password = ConvertTo-SecureString $Pwd -AsPlainText -Force
+$CredVM = New-Object System.Management.Automation.PSCredential ($UserName, $password)
 
 New-AzVm -ResourceGroupName $ResourceGroupName -Name $VMName -Location $location -Credential $CredVM
 
