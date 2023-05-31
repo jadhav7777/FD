aca91ef18203bdb5e643cd8abc2026730d6cc614
Abhishek_Jadhav_ <abhishek.jadhav@flexagon.com>
2023-05-31 12:43:57 +0530

    update.

--- PowerShell-Azure-Scripts/Create-Azure-VM.ps1
+++ PowerShell-Azure-Scripts/Create-Azure-VM.ps1
@@ -1,5 +1,8 @@
+Write-Host $Pwd
+Write-Host $UserName
+Write-Host $ResourceGroupName
 $password = ConvertTo-SecureString $Pwd -AsPlainText -Force
-$CredVM = New-Object System.Management.Automation.PSCredential ($UserName, $password)
+$CredVM = New-Object System.Management.Automation.PSCredential ('$UserName', $password)
 
 New-AzVm -ResourceGroupName $ResourceGroupName -Name $VMName -Location $location -Credential $CredVM
 
