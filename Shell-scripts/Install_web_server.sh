7a3bd322076b79cfdbcc291c26c4ed8872a0bd58
Abhishek_Jadhav_ <abhishek.jadhav@flexagon.com>
2023-05-31 23:35:54 +0530

    update.

--- Shell-scripts/Install_web_server.sh
+++ Shell-scripts/Install_web_server.sh
@@ -1,1 +1,2 @@
 az vm run-command invoke -g $resourcegroup -n $vmname --command-id RunPowerShellScript --scripts "Install-WindowsFeature -name Web-Server -IncludeManagementTools"
+az vm open-port --port 80 --resource-group $resourcegroup --name $vmname
