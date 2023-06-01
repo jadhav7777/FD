d4b740c51118aa06b85cdedae5d77a06810fcd17
Abhishek_Jadhav_ <abhishek.jadhav@flexagon.com>
2023-06-01 23:25:32 +0530

    update.

---
+++ EKS-Cluster/ekc_create_cluster_wait.sh
@@ -0,0 +1,2 @@
+aws eks create-cluster --name $clusterName --kubernetes-version 1.27  --role-arn $role_arn --resources-vpc-config subnetIds=subnet-c23fbd8f,subnet-71e4e318,subnet-f7c1868c,securityGroupIds=sg-47e75b2f,sg-8db408e5
+aws eks wait cluster-active --name $clusterName 
