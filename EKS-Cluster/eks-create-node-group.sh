32e64f1fb1944e332695242dc50dfd57c3e2d67a
Abhishek_Jadhav_ <abhishek.jadhav@flexagon.com>
2023-06-02 00:08:09 +0530

    update.

--- EKS-Cluster/eks-create-node-group.sh
+++ EKS-Cluster/eks-create-node-group.sh
@@ -1,2 +1,2 @@
 aws eks create-nodegroup --cluster-name $clusterName --nodegroup-name $nodeGroupName --node-role $role_arn  --ami-type  $amiType --instance-types $instanceType --subnets subnet-c23fbd8f --disk-size 20 
-aws eks wait nodegroup-active --name $nodeGroupName
+aws eks wait nodegroup-active --cluster-name $clusterName --nodegroup-name $nodeGroupName
