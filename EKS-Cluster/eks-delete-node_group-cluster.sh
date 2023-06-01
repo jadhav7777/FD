65b7c515788e818754a1898b497e109c16f281fb
Abhishek_Jadhav_ <abhishek.jadhav@flexagon.com>
2023-06-01 12:08:18 +0530

    update.

--- EKS-Cluster/eks-delete-node_group-cluster.sh
+++ EKS-Cluster/eks-delete-node_group-cluster.sh
@@ -1,2 +1,2 @@
-aws eks delete-nodegroup --nodegroup-name my-nodegroup --cluster-name demo-cluster
+aws eks delete-nodegroup --nodegroup-name eks-demo-group --cluster-name demo-cluster
 aws eks delete-cluster --name demo-cluster
