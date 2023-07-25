1839ca008d97f7f66199442ac698bb34bcc10e9e
Abhishek_Jadhav_ <abhishek.jadhav@flexagon.com>
2023-07-25 23:30:21 +0530

    Adding zip file.

--- AWS-LAMBDA/lambda-demo/lambda-demo.js
+++ AWS-LAMBDA/lambda-demo/lambda-demo.js
@@ -1,15 +1,35 @@
-exports.handler = async function(event) {
-  const { numberA, numberB} = event;
-  
-  function wait(){
-        return new Promise((resolve, reject) => {
-            setTimeout(() => resolve("Hello FD, Test zip file. Test 25-07-23-22-36"), 1000)
-        });
-    }
-    
-    console.log(await wait());
-  
-  return { 
-    "sumResult": numberA + numberB 
-  };
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-1' });
+
+const functionName = process.env.AWS_LAMBDA_FUNCTION_NAME;
+const encrypted = process.env['Password'];
+const userName = process.env['UserName'];
+let decrypted;
+
+
+function processEvent(event) {
 }
+
+exports.handler = async (event) => {
+  if (!decrypted) {
+    const kms = new AWS.KMS();
+    try {
+      const req = {
+        CiphertextBlob: Buffer.from(encrypted, 'base64'),
+        EncryptionContext: { LambdaFunctionName: functionName },
+      };
+      const data = await kms.decrypt(req).promise();
+      decrypted = data.Plaintext.toString('ascii');
+      console.log('Decrypted value of password:',decrypted );
+    } catch (err) {
+      console.log('Decrypt error:', err);
+      throw err;
+    }
+  }
+  processEvent(event);
+   const response = {
+        UserName: userName,
+        Password: decrypted
+    };
+    return response;
+};
