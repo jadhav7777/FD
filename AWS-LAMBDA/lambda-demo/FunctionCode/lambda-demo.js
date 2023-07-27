ea4df24c8f1e99b2c38c27ec7a8c823128f44323
Abhishek_Jadhav_ <abhishek.jadhav@flexagon.com>
2023-07-27 15:08:44 +0530

    .

--- AWS-LAMBDA/lambda-demo/FunctionCode/lambda-demo.js
+++ AWS-LAMBDA/lambda-demo/FunctionCode/lambda-demo.js
@@ -2,33 +2,43 @@ const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-1' });
 
 const functionName = process.env.AWS_LAMBDA_FUNCTION_NAME;
-const encrypted = process.env['Password'];
-const userName = process.env['UserName'];
-let decrypted;
-
+let password;
+let userName;
+let connectionString;
 
 function processEvent(event) {
 }
 
 exports.handler = async (event) => {
-  if (!decrypted) {
+
     const kms = new AWS.KMS();
     try {
-      const req = {
-        CiphertextBlob: Buffer.from(encrypted, 'base64'),
+      password = process.env['Password'];
+      userName = process.env['UserName'];
+      connectionString = process.env['Mysql_Connection_String'];
+      let req = {
+        CiphertextBlob: Buffer.from(password, 'base64'),
         EncryptionContext: { LambdaFunctionName: functionName },
       };
-      const data = await kms.decrypt(req).promise();
-      decrypted = data.Plaintext.toString('ascii');
+      let data = await kms.decrypt(req).promise();
+      password= data.Plaintext.toString('ascii');
+
+      req = {
+        CiphertextBlob: Buffer.from(connectionString, 'base64'),
+        EncryptionContext: { LambdaFunctionName: functionName },
+      };
+      data = await kms.decrypt(req).promise();
+      connectionString = data.Plaintext.toString('ascii');
     } catch (err) {
       console.log('Decrypt error:', err);
       throw err;
     }
-  }
+  
   processEvent(event);
    const response = {
         UserName: userName,
-        Password: decrypted
+        Password: password,
+        ConnectionString: connectionString
     };
     return response;
 };
